98c6094ebff02eb22968ba0867eb55817736b8cd
Adrien Meyrand <adrien.meyrand@gmail.com>
2024-11-21 20:30:06 +0100

    retour en arrière

--- config/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-$routes = [
-    ['method' => 'GET', 'path' => '/', 'handler' => 'HomeController@index'],
-    ['method' => 'GET', 'path' => '/home/test', 'handler' => 'HomeController@home'],
-    ['method' => 'GET', 'path' => '/home/users', 'handler' => 'UserController@getAll'],
-    ['method' => ['GET', 'POST'], 'path' => '/home/users/addUser', 'handler' => 'UserController@create'],
-    ['method' => ['GET', 'POST'], 'path' => '/home/users/updateUser', 'handler' => 'UserController@update']
-];
-define('ROUTES', $routes);
